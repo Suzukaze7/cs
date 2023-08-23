a4e4d00bc7771dbcfb3f3d563b992be8b744db8d
suzukaze <1027743497@qq.com>
2023-08-23 19:23:11 +0800

    skip archlab and finish cachelab

--- CSAPP/lab/cachelab/csim.c
+++ CSAPP/lab/cachelab/csim.c
@@ -1,7 +1,152 @@
+// author suzukaze
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<getopt.h>
+
 #include "cachelab.h"
 
-int main()
+char help_str[] = "Usage: ./csim-ref [-hv] -s <num> -E <num> -b <num> -t <file>\n\
+Options:\n\
+  -h         Print this help message.\n\
+  -v         Optional verbose flag.\n\
+  -s <num>   Number of set index bits.\n\
+  -E <num>   Number of lines per set.\n\
+  -b <num>   Number of block offset bits.\n\
+  -t <file>  Trace file.\n\
+\n\
+Examples:\n\
+  linux>  ./csim-ref -s 4 -E 1 -b 4 -t traces/yi.trace\n\
+  linux>  ./csim-ref -v -s 8 -E 2 -b 4 -t traces/yi.trace";
+
+struct Line
 {
-    printSummary(0, 0, 0);
+    int valid, tag, last;
+};
+
+int main(int argc, char **argv)
+{
+    int ver = 0, setb = 0, line = 0, offb = 0, opt;
+    char *trace = NULL;
+    while ((opt = getopt(argc, argv, "hvs:E:b:t:")) != -1)
+    {
+        switch (opt)
+        {
+        case 'h':
+            printf("%s", help_str);
+            return 0;
+        case 'v':
+            ver = 1;
+            break;
+        case 's':
+            setb = atoi(optarg);
+            break;
+        case 'E':
+            line = atoi(optarg);
+            break;
+        case 'b':
+            offb = atoi(optarg);
+            break;
+        case 't':
+            trace = optarg;
+            break;
+        default:
+            printf("%s", help_str);
+            return 0;
+        }
+    }
+
+    int setc = 1 << setb;
+    struct Line **cache = malloc(sizeof(struct Line *) * setc);
+    for (int i = 0; i < setc; i++)
+    {
+        cache[i] = malloc(sizeof(struct Line) * line);
+        for (int j = 0; j < line; j++)
+            cache[i][j].valid = 0;
+    }
+
+    int hit = 0, miss = 0, evict = 0;
+    char buf[1024];
+    FILE *fd = fopen(trace, "r");
+    for (int cur = 1; 1; cur++)
+    {
+        char op;
+        unsigned addr, size;
+        if (fscanf(fd, "%s %x,%u", &op, &addr, &size) == EOF)
+            break;
+
+        if (op == 'I')
+            continue;
+
+        sprintf(buf, "%c %x,%u", op, addr, size);
+
+        int tset = addr >> offb & ((1 << setb) - 1), tag = addr >> (offb + setb), flag = 0;
+        struct Line *target = cache[tset];
+        for (int i = 0; i < line; i++)
+            if (target[i].valid && target[i].tag == tag)
+            {
+                target[i].last = cur;
+                flag = 1;
+                break;
+            }
+
+        if (flag)
+        {
+            hit++;
+            if (ver)
+                printf("%s hit\n", buf);
+
+            if (op == 'M')
+            {
+                hit++;
+                if (ver)
+                    printf("%s hit\n", buf);
+            }
+            continue;
+        }
+
+        miss++;
+        if (ver)
+            printf("%s miss", buf);
+
+        int last = 0x3f3f3f3f, select;
+        flag = 0;
+        for (int i = 0; i < line; i++)
+            if (!target[i].valid)
+            {
+                select = i;
+                flag = 1;
+                break;
+            }
+            else if (target[i].last < last)
+            {
+                last = target[i].last;
+                select = i;
+            }
+
+        if (!flag)
+        {
+            evict++;
+            if (ver)
+                printf(" eviction");
+        }
+        target[select].tag = tag;
+        target[select].last = cur;
+        target[select].valid = 1;
+
+        if (op == 'M')
+        {
+            hit++;
+            if (ver)
+                printf(" hit");
+        }
+
+        if (ver)
+            puts("");
+    }
+
+    printSummary(hit, miss, evict);
+
     return 0;
 }
--- CSAPP/lab/cachelab/trans.c
+++ CSAPP/lab/cachelab/trans.c
@@ -1,4 +1,6 @@
-/* 
+// author: suzukaze
+
+/*
  * trans.c - Matrix transpose B = A^T
  *
  * Each transpose function must have a prototype of the form:
@@ -6,43 +8,115 @@
  *
  * A transpose function is evaluated by counting the number of misses
  * on a 1KB direct mapped cache with a block size of 32 bytes.
- */ 
+ */
 #include <stdio.h>
 #include "cachelab.h"
 
+#define get(id, off) t##id = A[k][j + off]
+#define set(id, off) B[j + off][k] = t##id
+
 int is_transpose(int M, int N, int A[N][M], int B[M][N]);
 
-/* 
+/*
  * transpose_submit - This is the solution transpose function that you
  *     will be graded on for Part B of the assignment. Do not change
  *     the description string "Transpose submission", as the driver
  *     searches for that string to identify the transpose function to
- *     be graded. 
+ *     be graded.
  */
 char transpose_submit_desc[] = "Transpose submission";
 void transpose_submit(int M, int N, int A[N][M], int B[M][N])
 {
+    int i, j, k, t0, t1, t2, t3, t4, t5, t6, t7;
+    if (M == 32)
+        for (i = 0; i < N; i += 8)
+        {
+            for (j = 0; j < M; j += 8)
+            {
+                for (k = i; k < i + 8; k++)
+                {
+                    get(0, 0), get(1, 1), get(2, 2), get(3, 3), get(4, 4), get(5, 5), get(6, 6), get(7, 7);
+                    set(0, 0), set(1, 1), set(2, 2), set(3, 3), set(4, 4), set(5, 5), set(6, 6), set(7, 7);
+                }
+            }
+        }
+    else if (M == 64)
+        for (i = 0; i < N; i += 4)
+        {
+            for (j = 0; j < M; j += 4)
+            {
+                for (k = i; k < i + 4; k++)
+                {
+                    get(0, 0), get(1, 1), get(2, 2), get(3, 3);
+                    set(0, 0), set(1, 1), set(2, 2), set(3, 3);
+                }
+            }
+        }
+    else
+        for (i = 0; i < N; i += 16)
+        {
+            for (j = 0; j < M; j += 16)
+            {
+                for (k = i; k < i + 16 && k < N; k++)
+                {
+                    get(0, 0), get(1, 1), get(2, 2), get(3, 3), get(4, 4), get(5, 5), get(6, 6), get(7, 7);
+                    set(0, 0), set(1, 1), set(2, 2), set(3, 3), set(4, 4), set(5, 5), set(6, 6), set(7, 7);
+                    if (M - j >= 16)
+                    {
+                        get(0, 8), get(1, 9), get(2, 10), get(3, 11), get(4, 12), get(5, 13), get(6, 14), get(7, 15);
+                        set(0, 8), set(1, 9), set(2, 10), set(3, 11), set(4, 12), set(5, 13), set(6, 14), set(7, 15);
+                    }
+                    else
+                    {
+                        get(0, 8), get(1, 9), get(2, 10), get(3, 11), get(4, 12);
+                        set(0, 8), set(1, 9), set(2, 10), set(3, 11), set(4, 12);
+                    }
+                }
+            }
+        }
+    // 为啥按 8 分块要比 16 分块要差
+    // for (i = 0; i < N; i += 8)
+    // {
+    //     for (j = 0; j < M; j += 8)
+    //     {
+    //         for (k = i; k < i + 8 && k < N; k++)
+    //         {
+    //             if (M - j >= 8)
+    //             {
+    //                 get(, 0), get(, 1), get(, 2), get(, 3), get(, 4), get(, 5), get(, 6), get(, 7);
+    //                 set(, 0), set(, 1), set(, 2), set(, 3), set(, 4), set(, 5), set(, 6), set(, 7);
+    //             }
+    //             else
+    //             {
+    //                 get(, 0), get(, 1), get(, 2), get(, 3), get(, 4), get(, 5);
+    //                 set(, 0), set(, 1), set(, 2), set(, 3), set(, 4), set(, 5);
+    //             }
+    //         }
+    //     }
+    // }
 }
 
-/* 
+/*
  * You can define additional transpose functions below. We've defined
- * a simple one below to help you get started. 
- */ 
-
-/* 
- * trans - A simple baseline transpose function, not optimized for the cache.
+ * a simple one below to help you get started.
  */
+
+ /*
+  * trans - A simple baseline transpose function, not optimized for the cache.
+  */
 char trans_desc[] = "Simple row-wise scan transpose";
 void trans(int M, int N, int A[N][M], int B[M][N])
 {
     int i, j, tmp;
 
-    for (i = 0; i < N; i++) {
-        for (j = 0; j < M; j++) {
+    for (i = 0; i < N; i++)
+    {
+        for (j = 0; j < M; j++)
+        {
             tmp = A[i][j];
             B[j][i] = tmp;
         }
-    }    
+    }
 
 }
 
@@ -56,14 +130,14 @@ void trans(int M, int N, int A[N][M], int B[M][N])
 void registerFunctions()
 {
     /* Register your solution function */
-    registerTransFunction(transpose_submit, transpose_submit_desc); 
+    registerTransFunction(transpose_submit, transpose_submit_desc);
 
     /* Register any additional transpose functions */
-    registerTransFunction(trans, trans_desc); 
+    registerTransFunction(trans, trans_desc);
 
 }
 
-/* 
+/*
  * is_transpose - This helper function checks if B is the transpose of
  *     A. You can check the correctness of your transpose by calling
  *     it before returning from the transpose function.
@@ -72,13 +146,15 @@ int is_transpose(int M, int N, int A[N][M], int B[M][N])
 {
     int i, j;
 
-    for (i = 0; i < N; i++) {
-        for (j = 0; j < M; ++j) {
-            if (A[i][j] != B[j][i]) {
+    for (i = 0; i < N; i++)
+    {
+        for (j = 0; j < M; ++j)
+        {
+            if (A[i][j] != B[j][i])
+            {
                 return 0;
             }
         }
     }
     return 1;
 }
-
