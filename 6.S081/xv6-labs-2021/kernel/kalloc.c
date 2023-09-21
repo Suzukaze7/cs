8436b86e0e30b9e2da32a6e1d0a01a4cb97745dc
suzukaze <1027743497@qq.com>
2023-09-21 21:32:27 +0800

    finish threadlab

--- 6.S081/xv6-labs-2021/kernel/kalloc.c
+++ 6.S081/xv6-labs-2021/kernel/kalloc.c
@@ -11,20 +11,14 @@
 
 void freerange(void *pa_start, void *pa_end);
 
-int flag;
-uint8 ref_cnt[(PHYSTOP - KERNBASE) / PGSIZE];
-
 extern char end[]; // first address after kernel.
-// defined by kernel.ld.
+                   // defined by kernel.ld.
 
-
-struct run
-{
+struct run {
   struct run *next;
 };
 
-struct
-{
+struct {
   struct spinlock lock;
   struct run *freelist;
 } kmem;
@@ -33,17 +27,16 @@ void
 kinit()
 {
   initlock(&kmem.lock, "kmem");
-  freerange(end, (void *)PHYSTOP);
+  freerange(end, (void*)PHYSTOP);
 }
 
 void
 freerange(void *pa_start, void *pa_end)
 {
   char *p;
-  p = (char *)PGROUNDUP((uint64)pa_start);
-  for (; p + PGSIZE <= (char *)pa_end; p += PGSIZE)
+  p = (char*)PGROUNDUP((uint64)pa_start);
+  for(; p + PGSIZE <= (char*)pa_end; p += PGSIZE)
     kfree(p);
-  flag = 1;
 }
 
 // Free the page of physical memory pointed at by v,
@@ -55,17 +48,13 @@ kfree(void *pa)
 {
   struct run *r;
 
-  if (((uint64)pa % PGSIZE) != 0 || (char *)pa < end || (uint64)pa >= PHYSTOP)
+  if(((uint64)pa % PGSIZE) != 0 || (char*)pa < end || (uint64)pa >= PHYSTOP)
     panic("kfree");
-
-  // check ref cnt
-  if (ref_cnt[REF_IDX((uint64)pa)] && --ref_cnt[REF_IDX((uint64)pa)])
-    return;
 
   // Fill with junk to catch dangling refs.
   memset(pa, 1, PGSIZE);
 
-  r = (struct run *)pa;
+  r = (struct run*)pa;
 
   acquire(&kmem.lock);
   r->next = kmem.freelist;
@@ -83,24 +72,11 @@ kalloc(void)
 
   acquire(&kmem.lock);
   r = kmem.freelist;
-  if (r)
+  if(r)
     kmem.freelist = r->next;
   release(&kmem.lock);
 
-  if (r)
-  {
-    memset((char *)r, 5, PGSIZE); // fill with junk
-    ref_cnt[REF_IDX((uint64)r)] = 1;
-  }
-  return (void *)r;
-}
-
-void kadd_ref_cnt(uint64 pa)
-{
-  ref_cnt[REF_IDX(pa)]++;
-}
-
-uint8 kget_ref_cnt(uint64 pa)
-{
-  return ref_cnt[REF_IDX(pa)];
+  if(r)
+    memset((char*)r, 5, PGSIZE); // fill with junk
+  return (void*)r;
 }
